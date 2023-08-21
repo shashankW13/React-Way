fc281f6724a565fe7189f74c579403f79ab80572
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-08-21 17:49:06 +0530

    Added to-do list jsx

---
+++ react-todo-list/src/App.jsx
@@ -0,0 +1,25 @@
+import "./styles.css"
+
+export default function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item"></input>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To-do List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
+}
