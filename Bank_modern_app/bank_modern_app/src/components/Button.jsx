662bf29fcd13b8c1a6e10c295819e3079a6328c0
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-08-28 18:24:13 +0530

    Added Business section

--- Bank_modern_app/bank_modern_app/src/components/Button.jsx
+++ Bank_modern_app/bank_modern_app/src/components/Button.jsx
@@ -1,7 +1,11 @@
-import React from "react"
+const Button = ({ styles }) => (
+	<button type="button" 
+			className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px]
+						text-primary outline-none ${styles}`}>
+		Get Started
+	</button>
+)
+	
 
-const Button = () => {
-	return <div>Button</div>
-}
 
 export default Button
